94e03cd9ea753a8615ae24f21c012bdd0caaa98f
phzineasygame <pedroflorencio@colegiopaulodetarso.com.br>
2025-11-05 15:32:58 +0000

    Rota usuário

--- app.py
+++ app.py
@@ -12,3 +12,9 @@ def info():
     modulo = "Flask"
     aula = "1"
     return  f"<h1>Módulo {modulo}, aula {aula}/h1>"
+
+@app.route('/bemvindo/<usuario>')
+
+def bemvindo(usuario):
+    
+    return  f"<h1> Bem vindo {usuario}/h1>"
